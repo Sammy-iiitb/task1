cb8cc43586d979534b2f422c61d92e07c902cf64
sammme <samyak.upadhyay123@gmail.com>
2016-06-08 15:22:02 +0530

    connect mysql to nodejs

---
+++ app.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "sitepoint"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.query('SELECT * FROM employess',function(err,rows){
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
